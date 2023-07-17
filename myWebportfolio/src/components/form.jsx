2eba765cdebffcd7e0f8e2c4cb413fedf9ed6d9a
JuanPablo <juanpablocappelletti@gmail.com>
2023-07-17 15:19:37 -0300

    terminando el css del form

--- myWebportfolio/src/components/form.jsx
+++ myWebportfolio/src/components/form.jsx
@@ -1,12 +1,26 @@
 import styles from './form.module.css';
+import btnstyles from './button.module.css'
 
 
 
 
 const Formulario = () => {
     return (
-        <section className={styles.form} id='contact'> 
-        
+        <section className={styles.contact} id='contact'> 
+        <h2 className='heading'>Contact <span>Me!</span></h2>
+
+        <form action='#'>
+            <div className={styles.input_box}>
+                <input type='text' placeholder='Full Name'></input>
+                <input type='email' placeholder='Email Address'></input>
+            </div>
+            <div className={styles.input_box}>
+                <input type='text' placeholder='Phone Number'></input>
+                <input type='text' placeholder='Email Subject'></input>
+            </div>
+            <textarea name='' id='' cols={30} rows={10} placeholder='Your Message'></textarea>
+            <input type='submit' value='Send Message' className={btnstyles.btn}></input>
+        </form>
     </section>
     )
 }
